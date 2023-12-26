2009a08eb30923a7bdeb53b7afc6d426f1782f33
destinydinam <destinydinam1@gmail.com>
2023-12-26 01:12:16 +0000

    maxlist

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -14,9 +15,25 @@ type ICDCode struct {
 	Name string `json:"name"`
 }
 
+type Result struct {
+	Total      int       `json:"total"`
+	Procedures []ICDCode `json:"procedures"`
+}
+
 func main() {
 	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		term := r.URL.Query().Get("terms")
+		term := strings.ToLower(r.URL.Query().Get("terms"))
+		maxListStr := r.URL.Query().Get("maxList")
+
+		maxList := 20 // default value
+		if maxListStr != "" {
+			var err error
+			maxList, err = strconv.Atoi(maxListStr)
+			if err != nil {
+				http.Error(w, "maxList must be an integer", http.StatusBadRequest)
+				return
+			}
+		}
 
 		file, err := os.Open("icd10pcs_codes_2023.txt")
 		if err != nil {
@@ -26,16 +43,16 @@ func main() {
 		defer file.Close()
 
 		scanner := bufio.NewScanner(file)
-		var matches []ICDCode
+		var allMatches []ICDCode
 		for scanner.Scan() {
 			line := scanner.Text()
 			splitIndex := strings.Index(line, " ")
 			code := ICDCode{
 				ID:   line[:splitIndex],
-				Name: line[splitIndex+1:],
+				Name: fmt.Sprintf("%s %s", line[:splitIndex], line[splitIndex+1:]),
 			}
-			if strings.Contains(strings.ToLower(code.ID), strings.ToLower(term)) || strings.Contains(strings.ToLower(code.Name), strings.ToLower(term)) {
-				matches = append(matches, code)
+			if strings.Contains(strings.ToLower(code.ID), term) || strings.Contains(strings.ToLower(code.Name), term) {
+				allMatches = append(allMatches, code)
 			}
 		}
 
@@ -44,9 +61,21 @@ func main() {
 			return
 		}
 
-		json.NewEncoder(w).Encode(matches)
+		result := Result{
+			Total:      len(allMatches),
+			Procedures: allMatches[:min(maxList, len(allMatches))],
+		}
+
+		json.NewEncoder(w).Encode(result)
 	})
 
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
